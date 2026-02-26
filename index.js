5309e2e46ce0387a913e1fec750b71dba31666e8
jason <nosajrarb2@gmail.com>
2026-02-26 22:48:31 +0530

    added post operation for users and user routing

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express")
-const app = express()
-const PORT = 2121
+import express from 'express'
+
+//routes
+import userRoutes from './routes/users.js'
+
+const app = express();
+const PORT = 2121;
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('🏠')
+    res.send('🏠');
+    console.log('🏠');
 })
+
+app.use('/users', userRoutes)
 
 app.listen(PORT, () => {console.log('Server: ✅')})
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {v4 as uuidv4} from 'uuid';
+const router = express.Router();
+
+const users = [
+    {name: 'nosajrarb', id: 1},
+    {name: 'irapbrar', id: 2}
+]
+
+router.get('/', (req, res) => {
+    res.send(users)
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push({...user, id: uuidv4()})
+    res.send(`${user} : ✅`)
+})
+
+export default router;
